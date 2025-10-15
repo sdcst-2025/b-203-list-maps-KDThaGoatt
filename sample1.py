16ff8641d378d07a386957d1d580515f099652c6
KDThaGoatt <kyand906@deltalearns.ca>
2025-10-15 10:34:34 -0700

    a

--- sample1.py
+++ sample1.py
@@ -8,6 +8,6 @@ def isEven(i):
     
 data = [76, 56, 20, 77, 57, 96, 24, 83, 16, 39, 25, 0, 86, 64, 65, 25, 15, 99, 77, 79, 31, 95, 25, 13, 9, 84, 42, 38, 9, 42]
 even = map(isEven, data)
-print(list(even))
+print(tuple(even))
 print(list(even))  # This will print an empty list since the iterator is exhausted
     
